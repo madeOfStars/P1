1354c46748199e631f57198b8b064a1ef2ee9407
Ertjon Mecka <ertjonmecka@gmail.com>
2017-04-12 14:01:29 +0200

    very minor change in edit release menu.

--- src/p1/frames/MainFrame.java
+++ src/p1/frames/MainFrame.java
@@ -26,11 +26,11 @@ import p1.utils.OperationsUtil;
  * @author Ertjon
  */
 public class MainFrame extends JFrame {
-    
+
     private final OperationsUtil op = OperationsUtil.getInstance();
     private JMenu prj;
     private JMenu rls;
-    
+
     public MainFrame() {
         setSize(700, 600);
         addWindowListener(new WindowCloser());
@@ -39,7 +39,7 @@ public class MainFrame extends JFrame {
         flowUtil.defineFirstView();
         setMenus();
     }
-    
+
     private void setMenus() {
         JMenuBar menuBar = new JMenuBar();
         MenuItemListener mListener = new MenuItemListener();
@@ -49,11 +49,11 @@ public class MainFrame extends JFrame {
         fileMenu.setName(MenuEnum.FILE.name());
         fileMenu.addMenuListener(ml);
         menuBar.add(fileMenu);
-        
+
         prj = new JMenu(MenuEnum.PROJECT.getLabel());
         prj.setFont(FontEnum.CONTENT.getFont());
         fileMenu.add(prj);
-        
+
         JMenuItem newPrj = new JMenuItem(MenuEnum.NEW_PRJ.getLabel());
         newPrj.setName(MenuEnum.NEW_PRJ.name());
         newPrj.setFont(FontEnum.CONTENT.getFont());
@@ -69,11 +69,11 @@ public class MainFrame extends JFrame {
         delPrj.setFont(FontEnum.CONTENT.getFont());
         prj.add(delPrj);
         delPrj.addActionListener(mListener);
-        
+
         rls = new JMenu(MenuEnum.RELEASE.getLabel());
         rls.setFont(FontEnum.CONTENT.getFont());
         fileMenu.add(rls);
-        
+
         JMenuItem newRls = new JMenuItem(MenuEnum.NEW_RLS.getLabel());
         newRls.setFont(FontEnum.CONTENT.getFont());
         newRls.setName(MenuEnum.NEW_RLS.name());
@@ -81,33 +81,34 @@ public class MainFrame extends JFrame {
         newRls.addActionListener(mListener);
         JMenuItem editRls = new JMenuItem(MenuEnum.EDIT_RLS.getLabel());
         editRls.setFont(FontEnum.CONTENT.getFont());
+        editRls.setName(MenuEnum.EDIT_RLS.name());
         rls.add(editRls);
         editRls.addActionListener(mListener);
         JMenuItem delRls = new JMenuItem(MenuEnum.DEL_RLS.getLabel());
         delRls.setFont(FontEnum.CONTENT.getFont());
         rls.add(delRls);
         delRls.addActionListener(mListener);
-        
+
         fileMenu.addSeparator();
         JMenuItem exit = new JMenuItem(MenuEnum.EXIT.getLabel());
         exit.setFont(FontEnum.CONTENT.getFont());
         exit.setName(MenuEnum.EXIT.name());
         fileMenu.add(exit);
         exit.addActionListener(mListener);
-        
+
         setJMenuBar(menuBar);
     }
-    
+
     private class WindowCloser extends WindowAdapter {
-        
+
         @Override
         public void windowClosing(WindowEvent event) {
             System.exit(0);
         }
     }
-    
+
     private class MenuItemListener implements ActionListener {
-        
+
         @Override
         public void actionPerformed(ActionEvent ae) {
             Object src = ae.getSource();
@@ -132,22 +133,24 @@ public class MainFrame extends JFrame {
                 }
             } else if (temp.getName().equals(MenuEnum.NEW_RLS.name())) {
                 FlowUtil.getInstance().addNewRelease();
+            } else if (temp.getName().equals(MenuEnum.EDIT_RLS.name())) {
+                
             } else if (temp.getName().equals(MenuEnum.EXIT.name())) {
                 System.exit(0);
             }
         }
-        
+
     }
-    
+
     private class MListener implements MenuListener {
-        
+
         @Override
         public void menuSelected(MenuEvent e) {
             Object src = e.getSource();
             JMenu temp = (JMenu) src;
             if (temp.getName().equals(MenuEnum.FILE.name())) {
                 switch (FlowUtil.getInstance().getActiveView()) {
-                    case HOME:{
+                    case HOME: {
                         prj.setEnabled(true);
                         rls.setEnabled(false);
                         break;
@@ -165,16 +168,16 @@ public class MainFrame extends JFrame {
                 }
             }
         }
-        
+
         @Override
         public void menuDeselected(MenuEvent e) {
-            
+
         }
-        
+
         @Override
         public void menuCanceled(MenuEvent e) {
-            
+
         }
-        
+
     }
 }
